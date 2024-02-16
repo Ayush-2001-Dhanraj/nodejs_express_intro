5ad3b742214fa9a695a6c2d43b63918ec08f021b
Ayush Dhanraj <dhanrajaayush123@gmail.com>
2024-02-16 16:28:33 +0530

    Add controls and public hbs

--- controller/messages.controller.js
+++ controller/messages.controller.js
@@ -1,7 +1,15 @@
+const path = require("path");
+
 const messages_model = require("../model/messages.model");
 
 function get_messages(req, res) {
-  return res.status(200).json(messages_model);
+  // return res
+  //   .status(200)
+  //   .sendFile(path.join(__dirname, "..", "public", "images", "cat.jpeg"));
+  res.render("message", {
+    title: "Messages to u mu kitty",
+    Author: "MoewPatra",
+  });
 }
 
 function post_message(req, res) {
--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require("express");
+const path = require("path");
 
 const friends_router = require("./routes/friends.router");
 const message_controller = require("./routes/messages.router");
 
 const PORT = 3000;
+
 const app = express();
+
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "views"));
 
 app.use((req, res, next) => {
   const start = Date.now();
@@ -16,6 +21,15 @@ app.use(express.json());
 
 app.use("/friends", friends_router);
 app.use("/messages", message_controller);
+
+app.use("/site", express.static(path.join(__dirname, "public")));
+
+app.use("/", (req, res) => {
+  res.render("index", {
+    title: "Lala lala!! MAWMA",
+    caption: "Meow MEOW purrr PURRRRRRRR........",
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`listening on ${PORT}`);
